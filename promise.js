d0b3018bd2609910b9e08e81b15f1d8dce0fdd28
rofiqirapsanjani <achmad.rofiqi@ui.ac.id>
2022-09-19 21:51:16 +0700

    jawaban

--- promise.js
+++ promise.js
@@ -1,7 +1,46 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
+
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  return new Promise((resolve, reject) => {
+    var countMarah = 0;
+    var countTidakMarah = 0; 
+    promiseTheaterVGC()
+    .then(
+      (data) => {
+
+        for (let i = 0; i < data.length; i++) {
+          if (data[i].hasil == "marah"){
+            countMarah = countMarah + 1;
+          } else if (data[i].hasil == "tidak marah"){
+            countTidakMarah = countTidakMarah + 1;
+          };
+        }
+      }
+    )
+    promiseTheaterIXX()
+    .then(
+      (data) => {
+        for (let i = 0; i < data.length; i++) {
+          if (data[i].hasil == "marah"){
+            countMarah = countMarah + 1;
+          } else if (data[i].hasil == "tidak marah"){
+            countTidakMarah = countTidakMarah + 1;
+          };
+        }
+        if (emosi === "marah"){
+          resolve(countMarah);
+        } else if (emosi === "tidak marah"){
+          resolve(countTidakMarah);
+        } else {
+          reject("error")
+        }
+      });
+    });
+}
+
+
 
 module.exports = {
   promiseOutput,
